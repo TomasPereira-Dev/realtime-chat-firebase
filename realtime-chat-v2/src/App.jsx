a490bbb8b49d6374adae5fdb6b6b9c18cd9e7e5d
Tomas Pereira <tomaspereira2k@gmail.com>
2023-02-28 20:45:44 -0300

    added postcss, new modal design, the app now runs on vite

---
+++ realtime-chat-v2/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import Modal from './Components/Modal/Modal';
+import Chat from "./Components/Chat/Chat";
+
+function App() {
+
+  return (
+    <>
+      <Modal/>
+      <Chat/>
+    </>
+
+  )
+}
+
+export default App
---
+++ realtime-chat-v2/src/Components/Chat/Chat.jsx
@@ -0,0 +1,11 @@
+import chatStyles from "./chat.module.css";
+
+const Chat = () =>{
+    return(
+        <div className={chatStyles.chatWrapper}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque impedit dolor ipsum asperiores aspernatur veritatis, quia odio hic pariatur eius. Laborum molestiae ducimus quis impedit praesentium obcaecati nesciunt quasi saepe.
+        </div>
+    )
+}
+
+export default Chat;
